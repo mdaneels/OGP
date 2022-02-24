aae4d346c3ac70a5ab23b0b6e0880e3270886af1
EvaHaanen <eva.haanen@student.kuleuven.be>
2022-02-24 15:09:22 +0100

    Add Constructor

--- src/File.java
+++ src/File.java
@@ -1,6 +1,16 @@
+import static java.lang.Integer.MAX_VALUE;
+
 public class File {
     private String name;
-    private long size;
+    private int size;
     private boolean writable;
+    private int maxSize = MAX_VALUE;
+
+    public File(String name, int size, boolean writable) {
+        //setName(name);
+        //setSize(size);
+        //setWritable(writable);
+        //setMaxSize(maxSize);
+    }
 
 }
