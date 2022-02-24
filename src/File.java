44b10a8d94863a335bfe16638f80d37a1fbecc6c
Arthur <arthur.cremelie@gmail.com>
2022-02-24 15:48:39 +0100

    Add init and change date

--- src/File.java
+++ src/File.java
@@ -1,16 +1,31 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import static java.lang.Integer.MAX_VALUE;
 
 public class File {
     private String name;
     private int size;
     private boolean writable;
-    private int maxSize = MAX_VALUE;
+    private Date creationDateTime;
+    private Date changeDateTime;
+
+
+    private final int maxSize = MAX_VALUE;
 
     public File(String name, int size, boolean writable) {
         //setName(name);
         //setSize(size);
         //setWritable(writable);
         //setMaxSize(maxSize);
+        //initCreationDateTime();
+    }
+
+    private void initCreationDateTime(){
+        this.creationDateTime = new Date();
+    }
+
+    private void changeCreationDateTime(){
+        this.changeDateTime = new Date();
     }
 
 }
