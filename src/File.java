fc5cd082ff843904d07e650ce0c177bb0a310ec3
EvaHaanen <eva.haanen@student.kuleuven.be>
2022-02-24 15:52:09 +0100

    Add enlarge (kinda)

--- src/File.java
+++ src/File.java
@@ -1,4 +1,5 @@
 import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.max;
 
 public class File {
     private String name;
@@ -13,4 +14,35 @@ public class File {
         //setMaxSize(maxSize);
     }
 
+    public void setSize(int size) {
+        assert isValidSize(size):
+                "Class Invariant: valid size";
+        this.size = size;
+    }
+
+    public boolean isValidSize(int size) {
+        return (size >= 0) && (size <= maxSize);
+    }
+
+    /**
+     * Enlage the size of the file with the given amount.
+     *
+     * @pre the amount must be larger than zero.
+     *
+     * @param amount
+     *        the amount of bytes to add to the size.
+     *
+     * @effect The new size of this file is equal to the old size incremented with the given amount of bytes.
+     * | new.getSize() = this.getSize() + amount
+     */
+    public void enlarge(int amount) {
+        assert canAcceptForEnlarge(amount) :
+                "Precondition: Acceptable amount for enlarge";
+        setSize(size + amount);
+        }
+    }
+
+    public boolean canAcceptForEnlarge(int amount) {
+        return ((amount > 0) && (isWritable()) && isValidSize(size + amount));
+    }
 }
