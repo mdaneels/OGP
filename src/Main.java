11a19138384868b6f1bd016caf3e4654d78eb407
Arthur <arthur.cremelie@gmail.com>
2022-03-09 16:08:34 +0100

    delete main contents

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,5 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException{
 
-        filee = new File("bumba");
-        Thread.sleep(100);
-        fileee = new File("loes");
-        Thread.sleep(100);
-
-        filee.setName("bumbaNieuw");
-        Thread.sleep(100);
-
-        fileee.setName("loesNieuw");
-        Thread.sleep(100);
-
-        filee.setName("bumbaOud");
-
-        System.out.println(filee.hasOverLappingUsePeriod(fileee));
-        System.out.println(fileee.hasOverLappingUsePeriod(filee));
-
     }
 }
